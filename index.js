d34ee1b8a96d263b78d757d556a7c489d6fff49a
victor.novakovets <victor.novakovets@cigility.com>
2019-10-10 17:00:57 +0300

    add card

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const exphbs = require("express-handlebars");
 const homeRout = require("./routes/home");
 const addRout = require("./routes/add");
 const coursesRout = require("./routes/courses");
+const CardRout = require("./routes/card");
 
 const app = express();
 
@@ -11,7 +12,7 @@ const hbs = exphbs.create({
     defaultLayoult: "main",
     extname: "hbs"
 });
-
+ 
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
@@ -19,9 +20,10 @@ app.set("views", "views");
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
-app.use(("/"),homeRout);
-app.use(("/add"),addRout);
-app.use(("/courses"),coursesRout);
+app.use(("/"), homeRout);
+app.use(("/add"), addRout);
+app.use(("/courses"), coursesRout);
+app.use(("/card"), CardRout);
 
 const PORT = process.env.port || 3000;
 
